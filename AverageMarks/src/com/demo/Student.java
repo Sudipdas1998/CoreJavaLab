8f662acd43d4d79978438040f205ec7774ed922c
ASUS <ASUS@LAPTOP-NKNKUGJK>
2022-10-07 11:26:16 +0530

    6th Sep Question No:3

---
+++ AverageMarks/src/com/demo/Student.java
@@ -0,0 +1,14 @@
+package com.demo;
+
+public class Student extends School {
+
+	public static void main(String[] args)
+
+	{
+
+		School s = new School();
+		s.average();
+		College c = new College();
+		c.average();
+	}
+}
